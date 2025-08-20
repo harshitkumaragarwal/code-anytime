ce3794318c86e263c75499b4ee36b604f6abfe47
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-20 19:22:22 +0530

    code running user age and sapid

--- username.c
+++ username.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main(){
     int userage;
+    int sapid;
     printf("pleass enter your age");
     scanf("%d",&userage);
-    printf("my age is %d year",userage);
+    printf("enter your sapid");
+    scanf("%d",&sapid);
+    printf("my age is %d year and my sapid is %d",userage,sapid);
     return 0;
 }
