6466c6d5d4c979fc680f78f70ae99683265e5f1e
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-20 20:38:47 +0530

    code sapid user

---
+++ sapid.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int userage;
+    int sapid;
+    printf("pleass enter your age");
+    scanf("%d",&userage);
+    printf("enter your sapid");
+    scanf("%d",&sapid);
+    printf("my age is %d year and my sapid is %d",userage,sapid);
+    return 0;
+}
