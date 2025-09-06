7b322abe005bdb10c20f3db7b0fae2df415826fd
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-09-06 23:50:38 +0530

    minus loop question

--- loop.c
+++ loop.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
 int main(){
-    int i;
-    double sum=0;
+    int i,min=0;
 
     for(i=1;i<=10;i++){
-        sum+=i;
+        min-=i;
+        printf("%d\n",min);
     }
-    printf("%lf\n",sum);
-
-    for(i=10;i>=1;i--){
-        sum+=i;
-    }
-    printf("%lf\n",sum);
-
     return 0;
 }
-
