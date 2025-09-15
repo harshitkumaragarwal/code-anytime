0d5cf9df25e33d99976d09df27dd9f204174c5c9
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-09-15 18:51:35 +0530

    hollow square

---
+++ 1234.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,j,num;
+
+    printf("enter the num");
+    scanf("%d",&num);
+
+    for(i=1;i<=5;i++){
+        for(j=1;j<=5;j++){
+            printf("i");
+        }
+        printf("\n");
+    }
+    return 0;
+}
